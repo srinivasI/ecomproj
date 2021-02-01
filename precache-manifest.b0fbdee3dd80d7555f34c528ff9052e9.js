1b276f3c162c726a3718651cb76b256ad7037626
Srinivasarao Ippili <ippilisrinivas715@gmail.com>
2021-02-01 06:11:46 +0530

    Updates

---
+++ precache-manifest.b0fbdee3dd80d7555f34c528ff9052e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4aa7bb3288ff06e4a68a",
+    "url": "/ecomproj/static/css/main.d5272c64.chunk.css"
+  },
+  {
+    "revision": "4aa7bb3288ff06e4a68a",
+    "url": "/ecomproj/static/js/main.118473ba.chunk.js"
+  },
+  {
+    "revision": "f03ee563552afc180aa9",
+    "url": "/ecomproj/static/js/runtime~main.73bba538.js"
+  },
+  {
+    "revision": "ba84f73396510bfb5f07",
+    "url": "/ecomproj/static/js/2.1bb01cdb.chunk.js"
+  },
+  {
+    "revision": "4290bc807f898ade8bc9a4296735ab10",
+    "url": "/ecomproj/static/media/newLogo.4290bc80.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/ecomproj/static/media/shopping-bag.09e6f5e7.svg"
+  },
+  {
+    "revision": "eef0439297dbf160a1e28bdc77a048d0",
+    "url": "/ecomproj/index.html"
+  }
+];
